768195b2b1ceffd916cbd348ba4c649630e43e01
DylanLangevin <77147936+DylanLangevin@users.noreply.github.com>
2024-06-02 21:04:10 +0200

    wip

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env sh
 
 set -e
 
@@ -5,17 +6,10 @@ npm run build
 
 cd dist
 
-if [ ! -d ".git" ]; then
-  git init
-fi
-
-
-git add .
-
-
+git init
+git add -A
 git commit -m 'deploy'
 
-
-git push -f git@github.com:dylanlangevin/studio-vue.git HEAD:gh-pages
+git push -f git@github.com:dylanlangevin/studio-vue.git main:gh-pages
 
 cd -
