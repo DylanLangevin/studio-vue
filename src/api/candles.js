1de0eeb9d45c523819d76ae322964835bd5163d0
DylanLangevin <77147936+DylanLangevin@users.noreply.github.com>
2024-06-03 16:25:21 +0200

    debug

--- src/api/candles.js
+++ src/api/candles.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef */
 const candles = [
-    { id: 1, name: 'Bougie', img: '@/assets/img/IMG_0333_1.png', desc: 'blablablablablaaaaaaaaaaaaaaaaaaaaaaaaaaaa' },
-    { id: 2, name: 'Bougie', img: '@/assets/img/IMG_0333_1.png', desc: 'blablablablabla' },
-    { id: 3, name: 'Bougie', img: '@/assets/img/IMG_0333_1.png', desc: 'blablablablabla' },
-    { id: 4, name: 'Bougie', img: '@/assets/img/IMG_0333_1.png', desc: 'blablablablabla' }
+    { id: 1, name: 'Bougie', img: '/assets/img/IMG_0333_1.png', desc: 'blablablablablaaaaaaaaaaaaaaaaaaaaaaaaaaaa' },
+    { id: 2, name: 'Bougie', img: '/assets/img/IMG_0333_1.png', desc: 'blablablablabla' },
+    { id: 3, name: 'Bougie', img: '/assets/img/IMG_0333_1.png', desc: 'blablablablabla' },
+    { id: 4, name: 'Bougie', img: '/assets/img/IMG_0333_1.png', desc: 'blablablablabla' }
 ]
 
 export default candles;
