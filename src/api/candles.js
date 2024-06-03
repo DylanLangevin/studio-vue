6b048ed1967764cb1adf2ad06656e6a0054bdeb3
DylanLangevin <77147936+DylanLangevin@users.noreply.github.com>
2024-06-03 15:40:47 +0200

    fix candlecard imgs

--- src/api/candles.js
+++ src/api/candles.js
@@ -1,8 +1,8 @@
 const candles = [
-    { id: 1, name: 'Bougie', img: 'src/assets/img/IMG_0333_1.png', desc: 'blablablablablaaaaaaaaaaaaaaaaaaaaaaaaaaaa' },
-    { id: 2, name: 'Bougie', img: 'src/assets/img/IMG_0333_1.png', desc: 'blablablablabla' },
-    { id: 3, name: 'Bougie', img: 'src/assets/img/IMG_0333_1.png', desc: 'blablablablabla' },
-    { id: 4, name: 'Bougie', img: 'src/assets/img/IMG_0333_1.png', desc: 'blablablablabla' }
+    { id: 1, name: 'Bougie', img: '../../assets/img/IMG_0333_1.png', desc: 'blablablablablaaaaaaaaaaaaaaaaaaaaaaaaaaaa' },
+    { id: 2, name: 'Bougie', img: '../../assets/img/IMG_0333_1.png', desc: 'blablablablabla' },
+    { id: 3, name: 'Bougie', img: '../../assets/img/IMG_0333_1.png', desc: 'blablablablabla' },
+    { id: 4, name: 'Bougie', img: '../../assets/img/IMG_0333_1.png', desc: 'blablablablabla' }
 ]
 
 export default candles;
